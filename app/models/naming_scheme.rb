89642f75bb03ecc27b0459164c3926532a9d6001
Bruz Marzolf <bmarzolf@systemsbiology.net>
2008-11-17 22:07:07 +0000

    Pull naming scheme to_csv exporter into SLIMarray.

--- app/models/naming_scheme.rb
+++ app/models/naming_scheme.rb
@@ -1,4 +1,6 @@
 class NamingScheme < ActiveRecord::Base
+  require 'csv'
+  
   has_many :naming_elements, :dependent => :destroy
   has_many :samples, :dependent => :destroy
   
@@ -18,5 +20,23 @@ class NamingScheme < ActiveRecord::Base
   def ordered_naming_elements
     return NamingElement.find(:all, :conditions => { :naming_scheme_id => id },
                                             :order => "element_order ASC" )
+  end
+  
+  def to_csv
+    csv_file_name = "#{RAILS_ROOT}/tmp/csv/#{SiteConfig.site_name}_naming_scheme_" +
+      "#{name}-#{Date.today.to_s}.csv"
+    
+    csv_file = File.open(csv_file_name, 'wb')
+    CSV::Writer.generate(csv_file) do |csv|
+      naming_elements.each do |ne|
+        if(ne.free_text == true)
+          csv << [ne.name, "FREE TEXT"]
+        else
+          csv << [ne.name] + ne.naming_terms.collect {|nt| nt.term}
+        end
+      end
+    end
+    
+    csv_file.close
   end
 end
