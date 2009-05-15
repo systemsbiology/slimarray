d47211c0ee6f712b0755654821d25c4154711924
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-05-15 11:02:14 -0700

    Add chip intergroup purchase model/controller/view.

---
+++ app/controllers/chip_intergroup_purchases_controller.rb
@@ -0,0 +1,40 @@
+class ChipIntergroupPurchasesController < ApplicationController
+  before_filter :login_required
+  before_filter :staff_or_admin_required
+  before_filter :populate_lab_group_and_chip_type_choices
+
+  def new
+    @chip_intergroup_purchase = ChipIntergroupPurchase.new(
+      :lab_group_id => params[:lab_group_id],
+      :chip_type_id => params[:chip_type_id]
+    )
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @chip_intergroup_purchase = ChipIntergroupPurchase.new(params[:chip_intergroup_purchase])
+
+    respond_to do |format|
+      # have to run valid? here to generate errors on Validatable attributes
+      if @chip_intergroup_purchase.valid? && @chip_intergroup_purchase.save
+        lab_group = LabGroup.find(@chip_intergroup_purchase.lab_group_id)
+        chip_type = ChipType.find(@chip_intergroup_purchase.chip_type_id)
+
+        flash[:notice] = 'Chip purchase was successfully created.'
+        format.html { redirect_to lab_group_chip_type_chip_transactions_url(lab_group,chip_type) }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
+  private
+
+  def populate_lab_group_and_chip_type_choices
+    @lab_groups = LabGroup.find(:all, :order => "name ASC")
+    @chip_types = ChipType.find(:all, :order => "name ASC")
+  end
+end
---
+++ app/models/chip_intergroup_purchase.rb
@@ -0,0 +1,50 @@
+class ChipIntergroupPurchase
+  include Validatable
+  
+  attr_accessor :date
+  attr_accessor :to_lab_group_id
+  attr_accessor :from_lab_group_id
+  attr_accessor :chip_type_id
+  attr_accessor :number
+
+  validates_presence_of :date, :to_lab_group_id, :from_lab_group_id, :chip_type_id
+  validates_numericality_of :number, :message => "of chips must be a number"
+
+  # initialize accepts an option hash with the following parameters:
+  #
+  # * <tt>date</tt> - The date of the transaction
+  # * <tt>buyer</tt> - The LabGroup buying arrays
+  # * <tt>seller</tt> - The LabGroup selling arrays
+  # * <tt>chip_type</tt> - The ChipType being bought
+  # * <tt>number</tt> - The number of arrays bought
+  def initialize(options = {})
+    @date = options[:date] || Date.today
+    @to_lab_group_id = options[:to_lab_group_id]
+    @from_lab_group_id = options[:from_lab_group_id]
+    @chip_type_id = options[:chip_type_id]
+    @number = options[:number] || 0
+  end
+
+  def save
+    to = LabGroup.find(@to_lab_group_id)
+    from = LabGroup.find(@from_lab_group_id)
+
+    buy_transaction = ChipTransaction.new(
+      :date => @date,
+      :lab_group_id => @to_lab_group_id,
+      :chip_type_id => @chip_type_id,
+      :description => "Purchased from #{from.name}",
+      :acquired => @number
+    )
+
+    sell_transaction = ChipTransaction.new(
+      :date => @date,
+      :lab_group_id => @from_lab_group_id,
+      :chip_type_id => @chip_type_id,
+      :description => "Purchased by #{to.name}",
+      :traded_sold => @number
+    )
+
+    buy_transaction.save && sell_transaction.save
+  end
+end
