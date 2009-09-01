53299d53812f080dcd5bfc8e77f624007aedde18
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-09-01 12:19:20 -0700

    Add 2dc_jqgrid plugin for AJAXy table management

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,50 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+  $("#bedata").click(function(){
+    var s; s = jQuery("#data_grid").getGridParam('selarrrow');
+    if( s != null ) {
+      if( s.length == 1) window.location.assign(location.href + "/" + s + "/edit");
+      else alert("Only one record can be edited at a time");
+    }
+    else alert("Please select a row to edit");
+  }); 
+
+  $("#bddata").click(function(){
+    var s; s = jQuery("#data_grid").getGridParam('selarrrow');
+    if( s == null || s.length == 0 ) alert("Please select a row to destroy");
+    else if( s.length > 1 ) alert("Only one record can be destroyed at a time");
+    else {
+      if( confirm("Are you sure you want to delete this record?") )
+        $('<form method="post" action="' + location.href + "/" + s + '" />')
+            .append('<input type="hidden" name="_method" value="delete" />')
+            .appendTo('body')
+            .submit();
+
+        return false;
+    } 
+  }); 
+
+  $("#besample").click(function(){
+    var s; s = jQuery("#sample_grid").getGridParam('selarrrow');
+    if( s != null ) {
+      if( s.length == 1) window.location.assign("/samples/" + s + "/edit");
+      else alert("Only one record can be edited at a time");
+    }
+    else alert("Please select a row to edit");
+  }); 
+
+  $("#bdsample").click(function(){
+    var s; s = jQuery("#sample_grid").getGridParam('selarrrow');
+    if( s == null || s.length == 0 ) alert("Please select a row to destroy");
+    else if( s.length > 1 ) alert("Only one record can be destroyed at a time");
+    else {
+      if( confirm("Are you sure you want to delete this record?") )
+        $('<form method="post" action="' + "/samples/" + s + '" />')
+            .append('<input type="hidden" name="_method" value="delete" />')
+            .appendTo('body')
+            .submit();
+
+        return false;
+    } 
+  }); 
+});
+ 
