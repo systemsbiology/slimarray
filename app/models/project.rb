a0f6178d8fa76bf41ea77ae3328519f6f4999ea3
Bruz Marzolf <bmarzolf@systemsbiology.org>
2009-09-03 09:43:01 -0700

    More efficient loading of attributes for Project#detail_hash

--- app/models/project.rb
+++ app/models/project.rb
@@ -74,8 +74,8 @@ class Project < ActiveRecord::Base
     return {
       :id => id,
       :name => name,
-      :lab_group => lab_group.name,
-      :lab_group_uri => "#{SiteConfig.site_url}/lab_groups/#{lab_group.id}",
+      :lab_group => lab_group_name,
+      :lab_group_uri => "#{SiteConfig.site_url}/lab_groups/#{lab_group_id}",
       :updated_at => updated_at,
       :sample_uris => sample_ids.sort.
         collect {|x| "#{SiteConfig.site_url}/samples/#{x}" }
