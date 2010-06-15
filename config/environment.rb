090e6e447a9801c8be6e871d7133acf8bd8389f4
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-06-15 13:03:21 -0700

    Use cookie_store instead of p_store, which helps towards Rails 2.3 upgrade

--- config/environment.rb
+++ config/environment.rb
@@ -48,7 +48,7 @@ Rails::Initializer.run do |config|
   # config.action_controller.session_store = :active_record_store
   
   # Use pstore, since we're storing > 4K in session
-  config.action_controller.session_store = :p_store
+  config.action_controller.session_store = :cookie_store
   
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
