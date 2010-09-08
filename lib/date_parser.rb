288a0c0154fbe4ba47e2573fdd7943af1c256e93
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-09-08 13:15:28 -0700

    Fix date parser to prevent exceptions when submitting incomplete chip transactions

--- lib/date_parser.rb
+++ lib/date_parser.rb
@@ -3,7 +3,7 @@ module DateParser
   def parse_date(year, month, day)
     return Date.today unless year && month && day
 
-    return "#{year}-#{month}-#{day}"
+    return Date.parse("#{year}-#{month}-#{day}")
   end
 
 end
