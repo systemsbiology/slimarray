c2c2b5604b17d296d1716374ceb18281f560732b
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-03-22 11:42:26 -0700

    Updates to raw data controller for schema reworking

--- app/controllers/raw_data_paths_controller.rb
+++ app/controllers/raw_data_paths_controller.rb
@@ -2,21 +2,21 @@ class RawDataPathsController < ApplicationController
   before_filter :staff_or_admin_required
 
   def create
-    @hybridization = Hybridization.find(
+    @microarray = Microarray.find(
       :first,
-      :include => {:microarray => :chip},
+      :include => :chip,
       :conditions => [ "chips.name = ? AND microarrays.array_number = ?",
                        params[:chip_name], params[:array_number] ]
     )
 
     respond_to do |format|
-      if @hybridization
-        @hybridization.update_attributes(:raw_data_path => params[:path])
-        format.xml  { render :xml => @hybridization, :status => :created, :location => @hybridization }
-        format.json  { render :json => @hybridization, :status => :created, :location => @hybridization }
+      if @microarray
+        @microarray.update_attributes(:raw_data_path => params[:path])
+        format.xml  { render :xml => @microarray, :status => :created }
+        format.json  { render :json => @microarray, :status => :created }
       else
-        format.xml  { render :xml => "No matching hybridization found", :status => 404 }
-        format.json  { render :json => "No matching hybridization found", :status => 404 }
+        format.xml  { render :xml => "No matching microarray found", :status => 404 }
+        format.json  { render :json => "No matching microarray found", :status => 404 }
       end
     end
   end
--- spec/controllers/raw_data_paths_controller_spec.rb
+++ spec/controllers/raw_data_paths_controller_spec.rb
@@ -9,23 +9,23 @@ describe RawDataPathsController do
 
   describe "POST 'create'" do
     it "should be successful with a valid chip name, array number and path" do
-      @hybridization = mock_model(Hybridization)
-      Hybridization.should_receive(:find).with(
+      @microarray = mock_model(Microarray)
+      Microarray.should_receive(:find).with(
         :first,
-        :include => {:microarray => :chip},
+        :include => :chip,
         :conditions => [ "chips.name = ? AND microarrays.array_number = ?",
                          "251485010001", "2" ]
-      ).and_return(@hybridization)
-      @hybridization.should_receive(:update_attributes).with(:raw_data_path => "/path/to/data.txt")
+      ).and_return(@microarray)
+      @microarray.should_receive(:update_attributes).with(:raw_data_path => "/path/to/data.txt")
 
       post :create, :chip_name => "251485010001", :array_number => 2, :path => "/path/to/data.txt"
       response.should be_success
     end
 
-    it "should not be successful when no matching hybridization is found" do
-      Hybridization.should_receive(:find).with(
+    it "should not be successful when no matching microarray is found" do
+      Microarray.should_receive(:find).with(
         :first,
-        :include => {:microarray => :chip},
+        :include => :chip,
         :conditions => [ "chips.name = ? AND microarrays.array_number = ?",
                          "251485010001", "2" ]
       ).and_return(nil)
