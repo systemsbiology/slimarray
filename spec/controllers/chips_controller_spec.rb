f4df929c8c873b15ae518d88157082bf891f3244
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-03-22 11:37:32 -0700

    Specs for chips controller

---
+++ spec/controllers/chips_controller_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe ChipsController do
+  include AuthenticatedSpecHelper
+
+  before(:each) do
+    login_as_user
+  end
+
+  describe "GET 'edit'" do
+    before(:each) do
+      @chip = mock_model(Chip)
+      @layout = mock("Layout")
+      @available_sample = mock("Available Samples")
+      Chip.stub!(:find).and_return(@chip)
+      @chip.stub!(:layout).and_return(@layout)
+      @chip.stub!(:available_samples).and_return(@available_samples)
+    end
+
+    it "finds the chip" do
+      Chip.should_receive(:find).with("23").and_return(@chip)
+      get 'edit', :id => "23"
+    end
+
+    it "generates the chip layout" do
+      @chip.should_receive(:layout).and_return(@layout)
+      get 'edit', :id => "23"
+    end
+      
+    it "loads available samples" do
+      @chip.should_receive(:available_samples).and_return(@available_samples)
+      get 'edit', :id => "23"
+    end
+  end
+
+  describe "PUT 'update'" do
+    before(:each) do
+      @chip = mock_model(Chip)
+      Chip.should_receive(:find).with("23").and_return(@chip)
+      @available_sample = mock("Available Samples")
+      @chip.stub!(:available_samples).and_return(@available_samples)
+    end
+
+    def do_put
+      put 'update', :id => "23", :chip => "Chip attributes"
+    end
+
+    describe "with valid parameters" do
+      before(:each) do
+        @chip.stub!(:update_attributes).and_return(true)
+        @chip.stub!(:layout)
+      end
+
+      it "updates the attributes successfully" do
+        @chip.should_receive(:update_attributes).with("Chip attributes").and_return(true)
+        do_put
+      end
+
+      it "loads the chip layout" do
+        @chip.should_receive(:layout)
+        do_put
+      end
+
+      it "renders the edit template" do
+        do_put
+        response.should render_template('edit')
+      end
+    end
+  end
+end
