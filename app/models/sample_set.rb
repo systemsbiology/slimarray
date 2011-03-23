8717f51f9e09925de8e647489dc183f77ec9c08c
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-03-23 09:49:49 -0700

    Remove old commented out code

--- app/models/sample_set.rb
+++ app/models/sample_set.rb
@@ -91,75 +91,6 @@ class SampleSet < ActiveRecord::Base
     end
   end
 
-  #def at_least_one_sample
-  #  errors.add(:samples, "must be provided") unless samples.size >= 1
-  #end
-
-#  # use validatable gem since we're validating attributes that aren't fields
-#  # in the sample_sets table (because they're transient)
-#  include Validatable
-#
-#  attr_accessor :submission_date, :number_of_samples, :project_id, :naming_scheme_id, :integer,
-#    :chip_type_id, :sbeams_user, :label_id, :service_option_id
-#
-#  validates_numericality_of :number_of_samples
-#  validates_presence_of :chip_type_id, :project_id, :service_option_id
-#  
-#  has_many :samples
-#  
-#  after_create :check_chip_inventory, :send_facility_notification, :send_approval_request
-#
-#  def self.new(attributes=nil)
-#    parse_multi_field_date(attributes)
-#    convert_to_integers(attributes)
-#
-#    sample_set = super
-#      
-#    # set the default label
-#    if attributes && attributes[:chip_type_id] && attributes[:chip_type_id] != ""
-#      sample_set.label_id = ChipType.find(attributes[:chip_type_id]).platform.default_label_id
-#    end
-#
-#    return sample_set
-#  end
-#
-#  def project
-#    Project.find_by_id(project_id)
-#  end
-#
-#  def chip_type
-#    ChipType.find_by_id(chip_type_id)
-#  end
-#
-#  def platform
-#    return chip_type.platform
-#  end
-#
-#  def naming_scheme
-#    NamingScheme.find_by_id(naming_scheme_id)
-#  end
-#
-#  def self.parse_multi_field_date(attributes)
-#    return unless attributes
-#
-#    # assume multi-field if year field is present
-#    if attributes["submission_date(1i)"]
-#      attributes["submission_date"] = Date.new(
-#        attributes.delete("submission_date(1i)").to_i,
-#        attributes.delete("submission_date(2i)").to_i,
-#        attributes.delete("submission_date(3i)").to_i
-#      )
-#    end
-#  end
-#
-#  def self.convert_to_integers(attributes)
-#    return unless attributes
-#
-#    attributes.each do |key, value|
-#      attributes[key] = Integer(value) rescue value
-#    end
-#  end
-
   def check_chip_inventory
     return if chips.empty?
 
@@ -171,10 +102,6 @@ class SampleSet < ActiveRecord::Base
     end
   end
 
-#  def service_option
-#    ServiceOption.find_by_id(service_option_id)
-#  end
-#
   def cost_estimate
     number.to_i * service_option.total_cost
   end
