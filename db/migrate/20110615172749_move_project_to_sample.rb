0f936e00086d17db589b76432695d91ab9cc4d6b
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-06-16 11:29:23 -0700

    Migration wasn't pulling project_id from sample sets correctly

--- db/migrate/20110615172749_move_project_to_sample.rb
+++ db/migrate/20110615172749_move_project_to_sample.rb
@@ -5,7 +5,11 @@ class MoveProjectToSample < ActiveRecord::Migration
     Sample.reset_column_information
     Sample.all.each do |sample|
       # ugly, but just for the sake of the migration
-      sample.update_attributes( :project_id => sample.try(:microarray).try(:chip).try(:sample_set).try(:project_id) )
+      sample_set = sample.try(:microarray).try(:chip).try(:sample_set)
+
+      # go through attributes since project_id is now an attr_accessor and won't give the value
+      # that's in the database
+      sample.update_attributes( :project_id => sample_set.attributes["project_id"] ) if sample_set
     end
 
     remove_column :sample_sets, :project_id
