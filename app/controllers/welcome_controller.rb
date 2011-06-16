c770d66851699e28c9a7a293cd0bf4827c715e2a
Bruz Marzolf <bmarzolf@systemsbiology.org>
2011-06-16 09:40:06 -0700

    Get rid of unneeded welcome/grid action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -36,37 +36,4 @@ class WelcomeController < ApplicationController
 
     @sample_sets = SampleSet.find(:all, :include => [:service_option, {:chips => {:microarrays => :samples}}], :conditions => "chips.status = 'submitted'")
   end
-
-  def grid
-    # Make an array of the accessible lab group ids, and use this
-    # to find the current user's accessible samples in a nice sorted list
-    lab_group_ids = current_user.get_lab_group_ids
-    projects = Project.find(:all, :conditions => [ "lab_group_id IN (?)", lab_group_ids],
-                              :order => "name ASC")
-    project_ids = projects.collect{|x| x.id}
-    #samples = Sample.find(:all, :conditions => [ "project_id IN (?) AND status = ?", project_ids, 'submitted' ],
-    #                          :order => "samples.id ASC")
-
-    samples = Sample.find(:all, :include => {:microarray => {:chip => {:sample_set => :project}}}) do
-      if params[:_search] == "true"
-        microarray.chip.sample_set.submission_date      =~ "%#{params[:submission_date]}%" if params[:submission_date].present?                
-        short_sample_name                               =~ "%#{params["short_sample_name"]}%" if params["short_sample_name"].present?
-        sample_name                                     =~ "%#{params[:sample_name]}%" if params[:sample_name].present?                
-        microarray.chip.sample_set.sbeams_user          =~ "%#{params[:sbeams_user]}%" if params[:sbeams_user].present?                
-        project.name                                    =~ "%#{params["projects.name"]}%" if params["projects.name"].present?                
-        microarray.chip.sample_set.service_option.name  =~ "%#{params["service_option.name"]}%" if params["service_option.name"].present?                
-        microarray.chip.sample_set.ready_for_processing =~ "%#{params["ready_for_processing"]}%" if params["ready_for_processing"].present?                
-      end
-      microarray.chip.status == "submitted"
-      "project_id IN (#{project_ids})"
-      paginate :page => params[:page], :per_page => params[:rows]      
-      order_by "#{params[:sidx]} #{params[:sord]}"
-    end
-
-    render :json => samples.to_jqgrid_json(
-      ["microarray.chip.sample_set.submission_date", :short_sample_name, :sample_name, "microarray.chip.sample_set.submitted_by",
-       "project.name", "microarray.chip.sample_set.service_option.name", "microarray.chip.sample_set.ready_yes_or_no"], 
-      params[:page], params[:rows], samples.total_entries
-    )
-  end
 end
